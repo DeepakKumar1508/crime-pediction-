1567405cdd696ab19e624ecbbc12c2a79e19b335
DeepakKumar1508 <deepakkumarkasarapu@gmail.com>
2026-06-10 14:05:47 +0000

    Added prediction page

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("Crime Prediction ML Project")
-    return "Crime Prediction Project is Running"
+    return """
+    <h1>Crime Prediction System</h1>
 
-if __name__ == "__main__":
-    app.run()
+    <form action="/predict">
+        Population:<br>
+        <input type="number" name="population"><br><br>
+
+        Unemployment Rate:<br>
+        <input type="number" name="unemployment"><br><br>
+
+        Poverty Rate:<br>
+        <input type="number" name="poverty"><br><br>
+
+        <input type="submit" value="Predict">
+    </form>
+    """
+
+@app.route("/predict")
+def predict():
+    unemployment = float(request.args.get("unemployment"))
+    poverty = float(request.args.get("poverty"))
+
+    if unemployment > 8 or poverty > 20:
+        return "<h2>⚠ High Crime Risk Area</h2>"
+    else:
+        return "<h2>✅ Low Crime Risk Area</h2>"
